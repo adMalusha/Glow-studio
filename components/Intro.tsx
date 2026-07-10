c20d0be32c988990a70585d20301a982ea5afac2
Aleks Malusha <133411188+adMalusha@users.noreply.github.com>
2026-07-10 13:47:34 +0200

    Update Intro.tsx

--- components/Intro.tsx
+++ components/Intro.tsx
@@ -8,9 +8,6 @@ const Intro: React.FC = () => {
         <div className="text-left md:text-center">
           <h2 className="text-4xl md:text-5xl serif-font text-[#5E503F] mb-12">Zorg voor elk <span className="italic">bijzonder</span> moment</h2>
           <div className="space-y-6 text-lg text-[#5E503F]/80 font-light leading-relaxed max-w-4xl mx-auto">
-            <p className="font-medium text-[#C98B73]">
-              Van een tastbare herinnering aan je zwangerschap tot verzorgende behandelingen met persoonlijke aandacht.
-            </p>
             <p>
               In ons salon kan je terecht voor 3D zwangerschapsbeeldjes, laserontharing en tanden witten. Alle behandelingen gaan door in een rustige omgeving, waar kwaliteit, comfort en vertrouwen centraal staan.
             </p>
