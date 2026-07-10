8aef415dcc6c16781c194583f686054b6bcfc5c3
Aleks Malusha <133411188+adMalusha@users.noreply.github.com>
2026-07-10 13:43:28 +0200

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { HashRouter as Router, Routes, Route, Link, useLocation } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Link, useLocation } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import StickyBookingButton from './components/StickyBookingButton';
